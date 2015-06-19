1632930525aa2b7d4fd3d52ca855768473d997c8
Yuriy <DeveloperYu@gmail.com>
2015-06-19 20:41:29 +0300

    nfc share and listen

--- www/app/components/invoices/invoices.js
+++ www/app/components/invoices/invoices.js
@@ -24,10 +24,54 @@
     var vm = this;
     vm.invoiceData = {};
     vm.accounts = [];
-    vm.lookUp = lookUp;
 
-    function lookUp(){
-      console.log('aaaaaa');
+    vm.lookUpMT = lookUpMT;
+
+    vm.shareMessage = shareMessage;
+
+    function shareMessage(){
+      if(window.nfc || (window.cordova && window.cordova.nfc)) {
+        var message = [
+            ndef.textRecord('{"test" : true }')
+        ];
+
+        nfc.share(
+          message,
+          function () { // success callback
+              alert("message shared");
+          },
+          function (error) { // error callback
+              alert("Error adding NDEF listener " + JSON.stringify(error));
+          }
+        );
+      } else {
+        console.log('no nfc bro :(');
+      }
+    }
+
+    function lookUpMT(){
+      // Read NDEF formatted NFC Tags
+      if(window.nfc || (window.cordova && window.cordova.nfc)) {
+        nfc.addMimeTypeListener(
+          'text/plain',
+          function (nfcEvent) {
+              var tag = nfcEvent.tag,
+                  ndefMessage = tag.ndefMessage;
+
+              // assuming the first record in the message has 
+              // a payload that can be converted to a string.
+              alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+          }, 
+          function () { // success callback
+              alert("Waiting for NDEF tag");
+          },
+          function (error) { // error callback
+              alert("Error adding NDEF listener " + JSON.stringify(error));
+          }
+        );
+      } else {
+        console.log('no nfc bro :(');
+      }
     }
   }
 }());
